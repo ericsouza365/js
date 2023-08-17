8386e544a1fc7597c8a347c1220c79d4cb3cd9c5
ericsouza365 <142313108+ericsouza365@users.noreply.github.com>
2023-08-17 13:42:46 -0300

    Add files via upload

---
+++ integracao.js
@@ -0,0 +1,20 @@
+var valor1=10;
+var valor2=3;
+
+//Operadores lógicos
+
+var valorCalculoSoma = valor1 + valor2;
+document.write("O primeiro valor  é " + valor1 + " o segundo valor é " + valor2 + " A soma desses valores é igual à ", + valorCalculoSoma,"<br>");
+
+var valorCalculoSubtracao = valor1 - valor2;
+document.write("O primeiro valor  é " + valor1 + " o segundo valor é " + valor2 + " A subtração desses valores é igual à " + valorCalculoSubtracao,"<br>");
+
+var valorCalculoMultiplicacao = valor1 * valor2;
+document.write("O primeiro valor  é " + valor1 + " o segundo valor é " + valor2 + " A multiplicação desses valores é igual à " + valorCalculoMultiplicacao,"<br>");
+
+var valorCalculoDivisao = valor1/valor2;
+document.write("O primeiro valor é " + valor1 + " o segundo valor é " + valor2 + "  A divisão desses valores é igual à " + valorCalculoDivisao,"<br>");
+
+var valorCalculoResto = valor1%valor2;
+document.write("O primeiro valor é " + valor1 + " o segundo valor é " + valor2 + "  O resto da divisão desses valores é igual à " + valorCalculoResto,"<br>");
+
